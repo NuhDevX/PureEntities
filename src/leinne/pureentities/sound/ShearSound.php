4a79da23adc331ed3ccd27bc0a41c478bec56a13
NuhDev <92098247+NuhDevX@users.noreply.github.com>
2026-01-17 15:39:43 +0700

    Update ShearSound.php

--- src/leinne/pureentities/sound/ShearSound.php
+++ src/leinne/pureentities/sound/ShearSound.php
@@ -7,11 +7,20 @@ namespace leinne\pureentities\sound;
 use pocketmine\math\Vector3;
 use pocketmine\network\mcpe\protocol\LevelSoundEventPacket;
 use pocketmine\world\sound\Sound;
+use pocketmine\network\mcpe\protocol\types\LevelSoundEvent;
+use pocketmine\entity\Entity;
 
 class ShearSound implements Sound{
+
+    public Entity $entity;
+    
+    public function __construct(Entity $entity){
+    $this->entity = $entity;
+    }
+
     public function encode(?Vector3 $pos) : array{
         return [
-            LevelSoundEventPacket::create(LevelSoundEventPacket::SOUND_SHEAR, $pos)
+            LevelSoundEventPacket::create(LevelSoundEvent::SHEAR, $pos, -1, $this->entity::getNetworkTypeId(), false, false, $this->entity->getId())
         ];
     }
 }
