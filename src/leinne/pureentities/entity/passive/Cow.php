3d8b5186b0655678ae09719d9c18785067c2d46c
NuhDev <92098247+NuhDevX@users.noreply.github.com>
2026-01-17 16:08:26 +0700

    Update Cow.php

--- src/leinne/pureentities/entity/passive/Cow.php
+++ src/leinne/pureentities/entity/passive/Cow.php
@@ -8,8 +8,6 @@ use leinne\pureentities\entity\Animal;
 use leinne\pureentities\entity\ai\walk\WalkEntityTrait;
 use pocketmine\entity\Entity;
 use pocketmine\entity\EntitySizeInfo;
-use pocketmine\item\ItemFactory;
-use pocketmine\item\ItemIds;
 use pocketmine\item\VanillaItems;
 use pocketmine\network\mcpe\protocol\types\entity\EntityIds;
 
@@ -46,10 +44,16 @@ class Cow extends Animal{
     }
 
     public function getDrops() : array{
-        return [
-            VanillaItems::LEATHER()->setCount(mt_rand(0, 2)),
-            ItemFactory::getInstance()->get($this->isOnFire() ? ItemIds::STEAK : ItemIds::RAW_BEEF, 0, mt_rand(1, 3)),
-        ];
+      $meat = $this->isOnFire()
+        ? VanillaItems::STEAK()
+        : VanillaItems::RAW_BEEF();
+
+       $meat->setCount(mt_rand(1, 3));
+
+       return [
+          VanillaItems::LEATHER()->setCount(mt_rand(0, 2)),
+          $meat
+       ];
     }
 
     public function getXpDropAmount() : int{
