734627ba29dc84c50ce6d2d1da98dd4a00fd33d2
NuhDev <92098247+NuhDevX@users.noreply.github.com>
2026-01-17 16:13:05 +0700

    Update Chicken.php

--- src/leinne/pureentities/entity/passive/Chicken.php
+++ src/leinne/pureentities/entity/passive/Chicken.php
@@ -8,8 +8,6 @@ use leinne\pureentities\entity\Animal;
 use leinne\pureentities\entity\ai\walk\WalkEntityTrait;
 use pocketmine\entity\Entity;
 use pocketmine\entity\EntitySizeInfo;
-use pocketmine\item\ItemFactory;
-use pocketmine\item\ItemIds;
 use pocketmine\item\VanillaItems;
 use pocketmine\network\mcpe\protocol\types\entity\EntityIds;
 
@@ -46,9 +44,15 @@ class Chicken extends Animal{
     }
 
     public function getDrops() : array{
+    $meat = $this->isOnFire()
+        ? VanillaItems::COOKED_CHICKEN()
+        : VanillaItems::RAW_CHICKEN();
+
+    $meat->setCount(mt_rand(1, 3));
+
         return [
-            ItemFactory::getInstance()->get($this->isOnFire() ? ItemIds::COOKED_CHICKEN : ItemIds::RAW_CHICKEN, 0, 1),
-            VanillaItems::FEATHER()->setCount(mt_rand(0, 2))
+           VanillaItems::FEATHER()->setCount(mt_rand(0, 2)),
+           $meat
         ];
     }
 
